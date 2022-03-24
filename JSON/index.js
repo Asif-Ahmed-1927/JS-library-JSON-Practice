488d3f6633b4f88a1af863cf2179a9b4ba370141
Asif Ahmed <asifbin8@gmail.com>
2022-03-24 14:35:02 +0600

    json

---
+++ JSON/index.js
@@ -0,0 +1,28 @@
+//const data= require("./friends_data.json");
+//console.log(data);
+
+//console.log(data.friends[0]);
+//console.log(data.friends[0].name)
+
+//data.friends[0].name="Anika"
+//console.log(data.friends[0].name);
+
+//delete data.friends[0].name;
+//console.log(data);
+
+//const data= require("./students_data.json");
+//console.log(data.students[1].language[0]);
+
+//for(x in data){
+    //console.log(x);
+    //console.log(data[x])
+//}
+
+//const data2={
+ //   name:"Asif",
+    //age: 26,
+//};
+
+//console.log(JSON.stringify(data2))
+
+console.log(JSON.parse('{"name": "Akash"}'))
